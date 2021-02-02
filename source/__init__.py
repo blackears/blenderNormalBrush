cab72edf3e4af4a3174dfecf9509e8081ffc805e
Mark McKay <mark@kitfox.com>
2021-02-02 05:54:31 -0500

    Adding license to header of source files.

--- source/__init__.py
+++ source/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Mark McKay
+# Copyright 2021 Mark McKay
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- source/ops/fixSeamNormals.py
+++ source/ops/fixSeamNormals.py
@@ -1,3 +1,18 @@
+#Copyright 2021 Mark McKay
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+
 import bpy
 import bpy.utils.previews
 import bmesh
@@ -5,21 +20,6 @@ import mathutils
 import math
 
 
-#---------------------------
-
-#class FixSeamNormalsSettings(bpy.types.PropertyGroup):
-##    operation_type : bpy.props.EnumProperty(
-##        items=(
-##            ('ACTIVE_TO_SELECTED', "Active", "Copy normals from active mesh to all selected ones"),
-##            ('COMBINED', "Combined", "Use faces of all selected meshes to calculate seam normal")
-##        ),
-##        default='ACTIVE_TO_SELECTED'
-##    )
-
-##    distance : bpy.props.FloatProperty(
-##        name="Distance", description="Maximum gap between vertices.", default = .000001, min=0, soft_max = .001
-##    )
-#    pass
 
 #---------------------------
 
@@ -50,30 +50,6 @@ class FixSeamNormalsOperator(bpy.types.Operator):
                         
         sum.normalize()
         return sum
-                        
-#                    for i, ek in enumerate(sel_mesh.edge_keys):
-#                        edge = sel_mesh.edges[i]
-#                        print ("edge " + str(edge))
-
-#                        v0 = sel_mesh.vertices[edge.vertices[0]]
-#                        v1 = sel_mesh.vertices[edge.vertices[1]]
-#                        
-#                        if v0.co == vertex.co:
-#                            off = mathutils.Vector(v1.co) - mathutils.Vector(v0.co)
-#                            
-                        
-                        
-##                        
-
-#        pass
-                    
-#                    for loop_index in p.loop_indices:
-#                        ls = sel_mesh.loops[loop_index]
-#                        vs = sel_mesh.vertices[ls.vertex_index]
-#                        #norm = ls.normal
-                        
-                        
-                        
                 
         
         
@@ -109,8 +85,6 @@ class FixSeamNormalsOperator(bpy.types.Operator):
         for obj in context.selected_objects:
             if obj.type == 'MESH' and obj != context.active_object:
                 sel_mesh = obj.data
-    
-#                print("OBJECT " + obj.name)
                 
                 sel_mesh.use_auto_smooth = True
                 normals = []
@@ -122,15 +96,11 @@ class FixSeamNormalsOperator(bpy.types.Operator):
                         
                         vs = sel_mesh.vertices[ls.vertex_index]
                         norm = ls.normal
-#                        print("norm  ls.index " + str(ls.index) + " vs idx:" + str(vs.index) + "  co " + str(vs.co) + "  norm " + str(ls.normal))
                         
                         for la in active_mesh.loops:
                             va = active_mesh.vertices[la.vertex_index]
-#                            print("passing vertex " + str(va.index) + " co " + str(va.co))
                             if (va.co == vs.co):
-#                                print("Match norm  va idx:" + str(va.index) + "  co " + str(va.co) + "  norm " + str(la.normal))
                                 norm = la.normal
-#                            break
                         normals.append(norm)
                     
                 sel_mesh.normals_split_custom_set(normals)
@@ -149,8 +119,6 @@ class FixSeamNormalsOperator(bpy.types.Operator):
                     vs = sel_mesh.vertices[ls.vertex_index]
                     if va.co == vs.co:
                         self.seam_verts.append(la)
-#                        print ("adding vert  va.index" + str(va.index) + " vs.index" + str(vs.index) + " co " + str(va.co))
-#                        print ("la.index " + str(la.index) + "  ls.index " + str(ls.index))
                         return
 
             
@@ -167,15 +135,8 @@ class FixSeamNormalsOperator(bpy.types.Operator):
         for la in active_mesh.loops:
             self.find_partner(context, active_mesh, la)
 
-#        operation_type = context.scene.fix_seam_normals_tool.operation_type
-
         self.active_to_selected(context, active_mesh)
                     
-#        if operation_type == "ACTIVE_TO_SELECTED":
-#            self.active_to_selected(context, active_mesh)
-#        else:
-#            self.combined(context, active_mesh)
-#    
         return {'FINISHED'}
         
 #---------------------------
@@ -195,14 +156,8 @@ class FixSeamNormalPropsPanel(bpy.types.Panel):
         layout = self.layout
 
         scene = context.scene
-#        settings = scene.fix_seam_normals_tool
-        
-
-#        row = layout.row();
-#        row.prop(settings, "operation_type", expand = True)
         
         col = layout.column();
-#        col.prop(settings, "distance")
         col.operator("kitfox.nt_fix_seam_normals", text="Copy Seam Normals")
         
 #---------------------------
@@ -229,16 +184,12 @@ class SeparateWithNormalsOperator(bpy.types.Operator):
 
 def register():
 
-#    bpy.utils.register_class(FixSeamNormalsSettings)
     bpy.utils.register_class(FixSeamNormalsOperator)
     bpy.utils.register_class(FixSeamNormalPropsPanel)
-
-#    bpy.types.Scene.fix_seam_normals_tool = bpy.props.PointerProperty(type=FixSeamNormalsSettings)
 
 
 
 def unregister():
-#    bpy.utils.unregister_class(FixSeamNormalsSettings)
     bpy.utils.unregister_class(FixSeamNormalsOperator)
     bpy.utils.unregister_class(FixSeamNormalPropsPanel)
 
